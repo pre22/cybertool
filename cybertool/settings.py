28c7ebbaf1f9cf53c6db2c7784fb64a7aa3717c0
Precious Chika <43546663+pre22@users.noreply.github.com>
2021-10-08 23:48:43 +0100

    chore: created tools app

--- cybertool/settings.py
+++ cybertool/settings.py
@@ -25,6 +25,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local app
+    'tools.apps.ToolsConfig',
+
+    # 3rd Party Apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
