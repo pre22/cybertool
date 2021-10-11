1c8f716e2a3ad1562c45445b2a03adef7295be46
Precious Chika <43546663+pre22@users.noreply.github.com>
2021-10-11 04:16:52 +0100

    fix: attributeerror fixed
    Working on typeError which occurred at line 19

--- tools/script.py
+++ tools/script.py
@@ -1,35 +1,47 @@
 import smtplib
 from email.mime.text import MIMEText
+from tools.forms import ToolsForm
 
-sender_input = str(input("Enter the Senders mail account here: "))
-receiver_input = str(input("Enter the link to txt file here: "))
-subject = str(input("Enter the subject here: "))
-body = str(input("Enter the link to the body_txt file here: "))
-password_user = str(input("Enter your password here: "))
+class Script(ToolsForm):
 
-body_txt = open(body, "r")
-body_content = body_txt.read()
-body_txt.close()
+    def __init__(self, sender_input=ToolsForm.declared_fields['sender_input'],
+                 receiver_input=ToolsForm.declared_fields['receiver_input'],
+                 subject=ToolsForm.declared_fields['subject'], body=ToolsForm.declared_fields['body'],
+                 password_user=ToolsForm.declared_fields['password_user']):
+        self.sender_input = sender_input
+        self.receiver_input = receiver_input
+        self.subject = subject
+        self.body = str(body)
+        self.password_user = password_user
+    
 
-text_content = open(receiver_input, "r")
-receiver_list = text_content.readlines()
-text_content.close()
-for user in receiver_list:
-    sender = sender_input
-    receivers = user
-    body_of_email = body_content
-    msg = MIMEText(body_of_email, "html")
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = user
+    def run(self):
+        body_txt = open(self.body, "r")
+        body_content = body_txt.read()
+        body_txt.close()
 
-    try:
-        s = smtplib.SMTP_SSL(host='smtp.gmail.com', port=465)
-        s.login(user=sender, password=password_user)
-        s.sendmail(sender, receivers, msg.as_string())
-        s.quit()
-        print("Sent to {}".format(user))
-    except smtplib.SMTPException:
-        continue
+        text_content = open(self.receiver_input, "r")
+        receiver_list = text_content.readlines()
+        text_content.close()
+        for user in receiver_list:
+            sender = self.sender_input
+            receivers = user
+            body_of_email = body_content
+            msg = MIMEText(body_of_email, "html")
+            msg['Subject'] = self.subject
+            msg['From'] = sender
+            msg['To'] = user
+
+            try:
+                s = smtplib.SMTP_SSL(host='smtp.gmail.com', port=465)
+                s.login(user=sender, password=self.password_user)
+                s.sendmail(sender, receivers, msg.as_string())
+                s.quit()
+                print("Sent to {}".format(user))
+            except smtplib.SMTPException:
+                continue
+
+        print("Ran Successful")
+
 
 
--- tools/views.py
+++ tools/views.py
@@ -1,10 +1,13 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 from tools.forms import ToolsForm
+from tools.script import Script
 
-
+scripts = Script()
 # Create your views here.
 class HomePageView(FormView):
     form_class = ToolsForm
     template_name = 'tools/home.html'
+    scripts.run()
     success_url = reverse_lazy('home')
+    
